4ec09f8279a1ba7a0bd74741467bc97a13af9b39
ZhaoXiaocong20 <469364022@qq.com>
2021-10-02 17:57:59 +0800

    update agent

--- Agent.py
+++ Agent.py
@@ -37,15 +37,15 @@ class Agent:
         self.velocity = velocity
         self.heading = heading
         self.target = target
-        # conducted observed_trajectory
+        # conducted trajectory
         self.observed_trajectory = np.array([[self.position[0],
                                               self.position[1],
                                               self.velocity[0],
                                               self.velocity[1],
                                               self.heading], ])
-        # observed_trajectory plan at each time step
+        # trajectory plan at each time step
         self.trj_solution = np.repeat([position], TRACK_LEN + 1, axis=0)
-        # collection of observed_trajectory plans at every time step
+        # collection of trajectory plans at every time step
         self.trj_solution_collection = []
         self.estimated_inter_agent = None
         self.ipv = 0
@@ -53,44 +53,8 @@ class Agent:
         self.ipv_error_collection = []
         self.virtual_track_collection = []
 
-    # def solve_game_KKT(self, inter_agent):
-    #     """
-    #     solve the game with an interacting counterpart
-    #     :param inter_agent: interacting counterpart [Agent]
-    #     :return:
-    #     """
-    #     # accessible info. on interacting counterpart:
-    #     #   1. observed_trajectory history: inter_agent.observed_trajectory
-    #     #   2. ipv guess: self.ipv_guess
-    #     self_info = [self.position,
-    #                  self.velocity,
-    #                  self.heading,
-    #                  self.ipv,
-    #                  self.target]
-    #     inter_info = [inter_agent.position,
-    #                   inter_agent.velocity,
-    #                   inter_agent.heading,
-    #                   self.ipv_guess,
-    #                   inter_agent.target]
-    #
-    #     fun = utility_KKT(self_info, inter_info)
-    #     cons = {'type': 'ineq', 'fun': con_inter_opt(self_info, inter_info)}
-    #
-    #     u0 = np.zeros([TRACK_LEN * 4])
-    #
-    #     bds = [(-MAX_ACCELERATION, MAX_ACCELERATION) for i in range(TRACK_LEN)] + \
-    #           [(-MAX_STEERING_ANGLE, MAX_STEERING_ANGLE) for i in range(TRACK_LEN)]
-    #     bds = bds + bds
-    #
-    #     res = minimize(fun, u0, bounds=bds, method='trust-constr', constraints=cons, options={'disp': True})
-    #     print(res.success)
-    #     x = np.reshape(res.x, [4, TRACK_LEN]).T
-    #     track_self = kinematic_model(x[:, 0:2], self_info[0:3])
-    #     track_inter = kinematic_model(x[:, 2:], inter_info[0:3])
-    #     print(x)
-    #     return track_self, track_inter
-
     def solve_game_IBR(self, inter_track):
+        track_len = np.size(inter_track, 0)
         self_info = [self.position,
                      self.velocity,
                      self.heading,
@@ -99,15 +63,16 @@ class Agent:
         p, v, h = self_info[0:3]
         init_state_4_kine = [p[0], p[1], v[0], v[1], h]
         fun = utility_IBR(self_info, inter_track)
-        u0 = np.zeros([TRACK_LEN * 2, 1])
-        bds = [(-MAX_ACCELERATION, MAX_ACCELERATION) for i in range(TRACK_LEN)] + \
-              [(-MAX_STEERING_ANGLE, MAX_STEERING_ANGLE) for i in range(TRACK_LEN)]
+        u0 = np.zeros([track_len * 2, 1])
+        bds = [(-MAX_ACCELERATION, MAX_ACCELERATION) for i in range(track_len)] + \
+              [(-MAX_STEERING_ANGLE, MAX_STEERING_ANGLE) for i in range(track_len)]
 
         res = minimize(fun, u0, bounds=bds, method='SLSQP')
 
         # print(res.success)
-        x = np.reshape(res.x, [2, TRACK_LEN]).T
-        self.trj_solution = kinematic_model(x, init_state_4_kine, TRACK_LEN, dt)
+        x = np.reshape(res.x, [2, track_len]).T
+        self.trj_solution = kinematic_model(x, init_state_4_kine, track_len, dt)
+        return self.trj_solution
 
     def interact_with_parallel_virtual_agents(self, agent_inter):
         """
@@ -119,12 +84,12 @@ class Agent:
 
         for ipv_temp in virtual_agent_IPV_range:
             # print('idx: ', ipv_temp)
+            virtual_inter_agent = copy.deepcopy(agent_inter)
+            virtual_inter_agent.ipv = ipv_temp
+            agent_self_temp = copy.deepcopy(self)
+
             count_iter = 0  # count number of iteration
             last_self_track = np.zeros_like(self.trj_solution)  # initialize a track reservation
-            virtual_inter_agent = copy.deepcopy(agent_inter)
-            agent_self_temp = copy.deepcopy(self)
-            virtual_inter_agent.ipv = ipv_temp
-
             while np.linalg.norm(agent_self_temp.trj_solution[:, 0:2] - last_self_track[:, 0:2]) > 1e-3:
                 count_iter += 1
                 last_self_track = agent_self_temp.trj_solution
@@ -175,33 +140,50 @@ class Agent:
             time_duration = current_time - start_time
 
             candidates = self.estimated_inter_agent.virtual_track_collection[start_time]
-            var = np.zeros(len(candidates))
 
+            virtual_track_collection = []
             for i in range(len(candidates)):
-                virtual_track = candidates[i][0:time_duration, 0:2]
-                actual_track = inter_agent.observed_trajectory[start_time:current_time, 0:2]
-                rel_dis = np.linalg.norm(virtual_track-actual_track, axis=0)
-                # likelihood of each candidate
-                var[i] = np.log10(np.prod((1/sigma/np.sqrt(2*math.pi))*np.exp(-rel_dis**2/(2*sigma**2))))
-                if var[i] < 0:
-                    var[i] = 0
+                virtual_track_collection.append(candidates[i][0:time_duration, 0:2])
+            actual_track = inter_agent.observed_trajectory[start_time:current_time, 0:2]
 
-            weight = var/(sum(var) + 1e-6)
-            print(weight)
+            ipv_weight = cal_reliability(actual_track, virtual_track_collection)
 
             # weighted sum of all candidates' IPVs
-            self.estimated_inter_agent.ipv = sum(virtual_agent_IPV_range * weight)
+            self.estimated_inter_agent.ipv = sum(virtual_agent_IPV_range * ipv_weight)
 
             # save updated ipv and estimation error
             self.estimated_inter_agent.ipv_collection.append(self.estimated_inter_agent.ipv)
-            error = 1 - np.sqrt(sum(weight ** 2))
+            error = 1 - np.sqrt(sum(ipv_weight ** 2))
             self.estimated_inter_agent.ipv_error_collection.append(error)
+
+    def estimate_self_ipv_in_NDS(self, inter_track, self_actual_track):
+        self_virtual_track_collection = []
+        ipv_range = np.random.normal(self.ipv, math.pi/6, 6)
+        for ipv_temp in ipv_range:
+            agent_self_temp = copy.deepcopy(self)
+            agent_self_temp.ipv = ipv_temp
+            # generate track with varied ipv
+            virtual_track_temp = agent_self_temp.solve_game_IBR(inter_track)
+            # save track into a collection
+            self_virtual_track_collection.append(virtual_track_temp)
+
+        # calculate reliability of each track
+        ipv_weight = cal_reliability(self_actual_track, self_virtual_track_collection)
+
+        # weighted sum of all candidates' IPVs
+        self.ipv = sum(ipv_range * ipv_weight)
+
+        # save updated ipv and estimation error
+        self.ipv_collection.append(self.estimated_inter_agent.ipv)
+        mean_ipv_range = np.mean(ipv_range)
+        error = np.sqrt(sum(ipv_weight * (ipv_range-mean_ipv_range) ** 2))
+        self.ipv_error_collection.append(error)
 
     def draw(self):
         cv_init_it, _ = get_central_vertices('lt')
         cv_init_gs, _ = get_central_vertices('gs')
         plt.plot(cv_init_gs[:, 0], cv_init_gs[:, 1], 'b--')
-        for t in range(TRACK_LEN):
+        for t in range(np.size(self.trj_solution, 0)):
             plt.plot(cv_init_it[:, 0], cv_init_it[:, 1], 'r-')
             plt.plot(cv_init_gs[:, 0], cv_init_gs[:, 1], 'b-')
             plt.plot(self.trj_solution[t, 0], self.trj_solution[t, 1], 'r*')
@@ -214,30 +196,7 @@ class Agent:
         plt.show()
 
 
-# def utility_KKT(self_info, inter_info):
-#     def fun(u):
-#         """
-#         Calculate the utility from the perspective of "self" agent
-#
-#         :param u: num_step by 4 array, where the 1\2 columns are control of "self" agent
-#                   and 3\4 columns "inter" agent
-#         :return: utility of the "self" agent under the control u
-#         """
-#         track_self = kinematic_model(u[0:TRACK_LEN * 2], self_info[0:3])
-#         track_inter = kinematic_model(u[TRACK_LEN * 2:], inter_info[0:3])
-#         track_all = [track_self[:, 0:2], track_inter[:, 0:2]]
-#
-#         group_cost = cal_group_cost(track_all)
-#         # print(group_cost)
-#         ut_self = np.cos(self_info[3]) * cal_interior_cost(track_self, self_info[4]) + \
-#                   np.sin(self_info[3]) * group_cost
-#         ut_inter = np.cos(inter_info[3]) * cal_interior_cost(track_inter, inter_info[4]) + \
-#                    np.sin(inter_info[3]) * group_cost
-#         return ut_self + ut_inter
-#     return fun
-
-
-def utility_IBR(self_info, inter_track):
+def utility_IBR(self_info, track_inter):
     def fun(u):
         """
         Calculate the utility from the perspective of "self" agent
@@ -248,9 +207,8 @@ def utility_IBR(self_info, inter_track):
         """
         p, v, h = self_info[0:3]
         init_state_4_kine = [p[0], p[1], v[0], v[1], h]
-        track_info_self = kinematic_model(u, init_state_4_kine, TRACK_LEN, dt)
+        track_info_self = kinematic_model(u, init_state_4_kine, np.size(track_inter, 0), dt)
         track_self = track_info_self[:, 0:2]
-        track_inter = inter_track
         track_all = [track_self, track_inter[:, 0:2]]
         # print(np.sin(self_info[3]))
         ut = np.cos(self_info[3]) * cal_interior_cost(track_self, self_info[4]) + \
@@ -281,7 +239,7 @@ def cal_interior_cost(track, target):
 
     "1. cost of travel delay"
     # calculate the on-reference distance of the given track (the longer the better)
-    travel_distance = np.linalg.norm(track[-1, 0:2] - track[0, 0:2]) / TRACK_LEN
+    travel_distance = np.linalg.norm(track[-1, 0:2] - track[0, 0:2]) / np.size(track, 0)
     cost_travel_distance = - travel_distance
     # print('cost of travel delay:', cost_travel_distance)
 
@@ -311,32 +269,26 @@ def cal_group_cost(track_packed):
     return cost_group * WEIGHT_GRP
 
 
-# def con_inter_opt(self_info, inter_info):
-#     """
-#     this is the constraint for KKT method
-#     :param self_info:
-#     :param inter_info:
-#     :return: constraint function
-#     """
-#
-#     def con(u):
-#         track_self = kinematic_model(u[0:TRACK_LEN * 2], self_info[0:3], TRACK_LEN, dt)
-#         # track_inter = kinematic_model(u[TRACK_LEN * 2:], inter_info[0:3])
-#         track_inter1 = kinematic_model(1.01 * u[TRACK_LEN * 2:], inter_info[0:3], TRACK_LEN, dt)
-#         track_inter2 = kinematic_model(0.99 * u[TRACK_LEN * 2:], inter_info[0:3], TRACK_LEN, dt)
-#
-#         ut_inter1 = np.cos(inter_info[3]) * cal_interior_cost(track_inter1, inter_info[4]) + \
-#                     np.sin(inter_info[3]) * cal_group_cost([track_self, track_inter1])
-#         ut_inter2 = np.cos(inter_info[3]) * cal_interior_cost(track_inter2, inter_info[4]) + \
-#                     np.sin(inter_info[3]) * cal_group_cost([track_self, track_inter2])
-#
-#         delta_ut = np.abs(ut_inter2 - ut_inter1)
-#         # print('delta_ut', MAX_DELTA_UT - delta_ut)
-#
-#         return MAX_DELTA_UT - delta_ut
-#         # return delta_ut
-#
-#     return con
+def cal_reliability(act_trck, vir_trck_coll):
+    """
+
+    :param act_trck: actual_track
+    :param vir_trck_coll: virtual_track_collection
+    :return:
+    """
+    candidates_num = len(vir_trck_coll)
+    var = np.zeros(candidates_num)
+    for i in range(candidates_num):
+        virtual_track = vir_trck_coll[i]
+        rel_dis = np.linalg.norm(virtual_track - act_trck, axis=0)
+        # likelihood of each candidate
+        var[i] = np.log10(np.prod((1 / sigma / np.sqrt(2 * math.pi)) * np.exp(-rel_dis ** 2 / (2 * sigma ** 2))))
+        if var[i] < 0:
+            var[i] = 0
+
+    weight = var / (sum(var) + 1e-6)
+    print(weight)
+    return weight
 
 
 if __name__ == '__main__':
