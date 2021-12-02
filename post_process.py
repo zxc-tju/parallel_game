fa1bde3048a0bd61ecbe658e0e258cdc1a9f3a71
Zhao Xiaocong <469364022@qq.com>
2021-12-02 13:44:32 +0800

    add PET calculation

--- post_process.py
+++ post_process.py
@@ -3,19 +3,19 @@ import math
 import gc
 import pandas as pd
 from matplotlib import pyplot as plt
-from tools.utility import get_central_vertices, smooth_cv
+from tools.utility import get_central_vertices, smooth_cv, savitzky_golay
 import numpy as np
 
 ipv_update_method = 1
-show_gif = 0
-save_fig = 1
+show_gif = 1
+save_fig = 0
 save_data = 0
 
 
-def show_results(gs_ipv, lt_ipv):
+def get_results(gs_ipv, lt_ipv):
 
     # import data
-    version_num = '19'
+    version_num = '23'
     filename = './outputs/version' + str(version_num) + '/data/agents_info' \
                + '_gs_' + str(gs_ipv) \
                + '_lt_' + str(lt_ipv) \
@@ -24,84 +24,127 @@ def show_results(gs_ipv, lt_ipv):
     agent_lt, agent_gs = pickle.load(f)
     f.close()
 
-    "save data to excel"
+    "====data abstraction===="
+    # lt track (observed and planned)
+    lt_ob_trj = agent_lt.observed_trajectory[:, 0:2]
+    lt_trj_coll = agent_lt.trj_solution_collection[0][:, 0:2]
+    for i in range(9):
+        coll_temp = agent_lt.trj_solution_collection[(i + 1) * 2][:, 0:2]
+        lt_trj_coll = np.concatenate([lt_trj_coll, coll_temp], axis=1)
+    v_lt = np.linalg.norm(agent_lt.observed_trajectory[:, 2:4], axis=1)
+    v_lt_smoothed = savitzky_golay(v_lt, 5, 3)
+
+    # gs track (observed and planned)
+    gs_ob_trj = agent_gs.observed_trajectory[:, 0:2]
+    gs_trj_coll = agent_gs.trj_solution_collection[0][:, 0:2]
+    for i in range(9):
+        coll_temp = agent_gs.trj_solution_collection[(i + 1) * 2][:, 0:2]
+        gs_trj_coll = np.concatenate([gs_trj_coll, coll_temp], axis=1)
+    v_gs = np.linalg.norm(agent_gs.observed_trajectory[:, 2:4], axis=1)
+    v_gs_smoothed = savitzky_golay(v_gs, 5, 3)
+    # link from gs to lt
+    link = np.concatenate([[lt_ob_trj[0, :]], [gs_ob_trj[0, :]]])
+    for i in range(10):
+        link = np.concatenate([link, np.concatenate([[lt_ob_trj[(i + 1) * 2, :]],
+                                                     [gs_ob_trj[(i + 1) * 2, :]]])], axis=1)
+    # process data for figures
+    cv_lt, progress_lt = get_central_vertices('lt', None)
+    cv_gs, progress_gs = get_central_vertices('gs', None)
+
+    "====calculate PET===="
+    # find longitudinal position of conflict point
+    conflict_point = np.array([13, -2])
+    dis2cv_lt_cp = cv_lt - conflict_point
+    dis2cv_lt_cp_norm = np.linalg.norm(dis2cv_lt_cp, axis=1)
+    dis2cv_lt_cp_min = np.amin(dis2cv_lt_cp_norm)
+    cv_index_lt_cp = np.where(dis2cv_lt_cp_min == dis2cv_lt_cp_norm)
+    long_progress_lt_cp = progress_lt[cv_index_lt_cp]
+
+    dis2cv_gs_cp = cv_gs - conflict_point
+    dis2cv_gs_cp_norm = np.linalg.norm(dis2cv_gs_cp, axis=1)
+    dis2cv_gs_cp_min = np.amin(dis2cv_gs_cp_norm)
+    cv_index_gs_cp = np.where(dis2cv_gs_cp_min == dis2cv_gs_cp_norm)
+    long_progress_gs_cp = progress_gs[cv_index_gs_cp]
+
+    # find longitudinal progress at each time point
+    ttcp_lt = []
+    ttcp_gs = []
+    pet = []
+    for t in range(len(lt_ob_trj)):
+        dis2cv_lt = cv_lt - lt_ob_trj[t, 0:2]
+        dis2cv_lt_norm = np.linalg.norm(dis2cv_lt, axis=1)
+        dis2cv_lt_min = np.amin(dis2cv_lt_norm)
+        cv_index_lt = np.where(dis2cv_lt_min == dis2cv_lt_norm)
+        long_progress_lt = progress_lt[cv_index_lt]
+        ttcp_lt.append((long_progress_lt_cp - long_progress_lt)/v_lt_smoothed[t])
+
+        dis2cv_gs = cv_gs - gs_ob_trj[t, 0:2]
+        dis2cv_gs_norm = np.linalg.norm(dis2cv_gs, axis=1)
+        dis2cv_gs_min = np.amin(dis2cv_gs_norm)
+        cv_index_gs = np.where(dis2cv_gs_min == dis2cv_gs_norm)
+        long_progress_gs = progress_gs[cv_index_gs]
+        ttcp_gs.append((long_progress_gs_cp - long_progress_gs) / v_gs_smoothed[t])
+
+        if ttcp_lt[-1] > 0 and ttcp_gs[-1] > 0:
+            temp_pet = np.abs(ttcp_gs[-1] - ttcp_lt[-1])
+            pet.append(float(temp_pet))
+        else:
+            pet.append(0)
+    pet = np.array(pet)
+    "====save data to excel===="
     if save_data:
-        lt_ob_trj = agent_lt.observed_trajectory[:, 0:2]
         df_lt_ob_trj = pd.DataFrame(lt_ob_trj)
-
-        lt_trj_coll = agent_lt.trj_solution_collection[0][:, 0:2]
-        for i in range(9):
-            coll_temp = agent_lt.trj_solution_collection[(i+1)*2][:, 0:2]
-            lt_trj_coll = np.concatenate([lt_trj_coll, coll_temp], axis=1)
         df_lt_trj_coll = pd.DataFrame(lt_trj_coll)
-
-        gs_ob_trj = agent_gs.observed_trajectory[:, 0:2]
         df_gs_ob_trj = pd.DataFrame(gs_ob_trj)
-
-        gs_trj_coll = agent_gs.trj_solution_collection[0][:, 0:2]
-        for i in range(9):
-            coll_temp = agent_gs.trj_solution_collection[(i+1)*2][:, 0:2]
-            gs_trj_coll = np.concatenate([gs_trj_coll, coll_temp], axis=1)
         df_gs_trj_coll = pd.DataFrame(gs_trj_coll)
-
-        link = np.concatenate([[lt_ob_trj[0, :]], [gs_ob_trj[0, :]]])
-        for i in range(10):
-            link = np.concatenate([link,
-                                   np.concatenate([[lt_ob_trj[(i+1)*2, :]], [gs_ob_trj[(i+1)*2, :]]])], axis=1)
         df_link = pd.DataFrame(link)
+        df_pet = pd.DataFrame(pet)
 
         with pd.ExcelWriter('outputs/excel/' + str(version_num) + '/output'
                             + '_gs_' + str(gs_ipv)
                             + '_lt_' + str(lt_ipv) + '.xlsx') as writer:
             df_lt_ob_trj.to_excel(writer, index=False, sheet_name='lt_ob_trj')
-            df_lt_trj_coll.to_excel(writer, index=False, sheet_name='lt_trj_coll')
             df_gs_ob_trj.to_excel(writer, index=False, sheet_name='gs_ob_trj')
+            df_lt_trj_coll.to_excel(writer, index=False, sheet_name='lt_trj_coll')
             df_gs_trj_coll.to_excel(writer, index=False, sheet_name='gs_trj_coll')
             df_link.to_excel(writer, index=False, sheet_name='link')
+            df_pet.to_excel(writer, index=False, sheet_name='PET')
 
-    # process data for figures
-    cv_init_it, _ = get_central_vertices('lt', None)
-    cv_init_gs, _ = get_central_vertices('gs', None)
+    if save_fig or show_gif:
 
-    if hasattr(agent_lt, 'trajectory'):
-        agent_lt_observed_trajectory = agent_lt.trajectory
-        agent_gs_observed_trajectory = agent_gs.trajectory
-    else:
-        agent_lt_observed_trajectory = agent_lt.observed_trajectory
-        agent_gs_observed_trajectory = agent_gs.observed_trajectory
-
-    "====final observed_trajectory===="
-    if show_gif:
-        fig = plt.figure(figsize=(12, 28))  # for showing gif
+        # set figure
+        fig = plt.figure(figsize=(12, 12))  # for showing gif
     else:
         fig = plt.figure(dpi=300, figsize=(12, 18))  # for printing figure
-    ax1 = plt.subplot(211)
-    ax2 = fig.add_subplot(212)
+    ax1 = plt.subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
     img = plt.imread('background_pic/T_intersection.jpg')
 
-    num_frame = len(agent_lt_observed_trajectory)
+    "====final observed_trajectory===="
+    num_frame = len(lt_ob_trj)
     for t in range(num_frame):
         ax1.cla()
         ax1.imshow(img, extent=[-9.1, 24.9, -13, 8])
         ax1.set(xlim=[-9.1, 24.9], ylim=[-13, 8])
         # central vertices
-        ax1.plot(cv_init_it[:, 0], cv_init_it[:, 1], 'r-')
-        ax1.plot(cv_init_gs[:, 0], cv_init_gs[:, 1], 'b-')
+        ax1.plot(cv_lt[:, 0], cv_lt[:, 1], 'r-')
+        ax1.plot(cv_gs[:, 0], cv_gs[:, 1], 'b-')
         # left-turn
-        ax1.scatter(agent_lt_observed_trajectory[:t + 1, 0],
-                    agent_lt_observed_trajectory[:t + 1, 1],
+        ax1.scatter(lt_ob_trj[:t + 1, 0],
+                    lt_ob_trj[:t + 1, 1],
                     s=120,
                     alpha=0.4,
                     color='red',
                     label='left-turn')
         # go-straight
-        ax1.scatter(agent_gs_observed_trajectory[:t + 1, 0],
-                    agent_gs_observed_trajectory[:t + 1, 1],
+        ax1.scatter(gs_ob_trj[:t + 1, 0],
+                    gs_ob_trj[:t + 1, 1],
                     s=120,
                     alpha=0.4,
                     color='blue',
                     label='go-straight')
-        if t < len(agent_lt_observed_trajectory) - 1:
+        if t < len(lt_ob_trj) - 1:
             # real-time virtual plans of ## ego ## at time step t
             lt_track = agent_lt.trj_solution_collection[t]
             ax1.plot(lt_track[:, 0], lt_track[:, 1], '--', linewidth=3)
@@ -116,16 +159,16 @@ def show_results(gs_ipv, lt_ipv):
                 for track_gs in candidates_gs:
                     ax1.plot(track_gs[:, 0], track_gs[:, 1], color='green', alpha=0.5)
         # position link
-        ax1.plot([agent_lt_observed_trajectory[t, 0], agent_gs_observed_trajectory[t, 0]],
-                 [agent_lt_observed_trajectory[t, 1], agent_gs_observed_trajectory[t, 1]],
+        ax1.plot([lt_ob_trj[t, 0], gs_ob_trj[t, 0]],
+                 [lt_ob_trj[t, 1], gs_ob_trj[t, 1]],
                  color='gray',
                  alpha=0.2)
         if show_gif:
-            plt.pause(0.3)
+            plt.pause(0.1)
     # full position link
-    for t in range(len(agent_lt_observed_trajectory)):
-        ax1.plot([agent_lt_observed_trajectory[t, 0], agent_gs_observed_trajectory[t, 0]],
-                 [agent_lt_observed_trajectory[t, 1], agent_gs_observed_trajectory[t, 1]],
+    for t in range(len(lt_ob_trj)):
+        ax1.plot([lt_ob_trj[t, 0], gs_ob_trj[t, 0]],
+                 [lt_ob_trj[t, 1], gs_ob_trj[t, 1]],
                  color='gray',
                  alpha=0.1)
     ax1.set_title('gs_' + str(gs_ipv) + '_lt_' + str(lt_ipv), fontsize=12)
@@ -147,7 +190,7 @@ def show_results(gs_ipv, lt_ipv):
         y_error_lt = np.array(agent_lt.estimated_inter_agent.ipv_error_collection)
         error_smoothed_lt, _ = smooth_cv(np.array([x_range, y_error_lt]).T)
         y_error_lt_smoothed = error_smoothed_lt[:, 1]
-        plt.fill_between(x_smoothed_lt, y_lt_smoothed - y_error_lt_smoothed, y_lt_smoothed + y_error_lt_smoothed,
+        ax2.fill_between(x_smoothed_lt, y_lt_smoothed - y_error_lt_smoothed, y_lt_smoothed + y_error_lt_smoothed,
                          alpha=0.3,
                          color='blue',
                          label='estimated gs IPV')
@@ -180,6 +223,12 @@ def show_results(gs_ipv, lt_ipv):
              linewidth=5,
              label='actual lt IPV')
 
+    "====PET===="
+    ax3.plot(pet)
+
+    if show_gif:
+        plt.show()
+
     # save figure
     if save_fig:
         plt.savefig('./outputs/version' + str(version_num) + '/figures/'
@@ -191,9 +240,9 @@ def show_results(gs_ipv, lt_ipv):
 
 
 if __name__ == '__main__':
-    # ipv_list = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+    # ipv_list = [-3, -2, -1, 0, 1, 2, 3]
     ipv_list = [-2, 0, 2]
     # ipv_list = [1]
-    for gs in ipv_list:
+    for gs in [2]:
         for lt in ipv_list:
-            show_results(gs, lt)
+            get_results(gs, lt)
